869c28a039f0984e917a790c91ab0cb306c3b345
MiladSadeghi <miladpci88@gmail.com>
2024-07-08 13:26:42 +0330

    init project

---
+++ src/main/java/com/miladsadeghi/springelasticapp/SpringElasticAppApplication.java
@@ -0,0 +1,13 @@
+package com.miladsadeghi.springelasticapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringElasticAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringElasticAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miladsadeghi/springelasticapp/SpringElasticAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miladsadeghi.springelasticapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringElasticAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
